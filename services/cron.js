954d67aa59667d6584c47bac761b06a47da98253
aab11 <ahmed@monbanquet.fr>
2018-08-07 00:05:51 +0100

    Fix CRON minutes field

--- services/cron.js
+++ services/cron.js
@@ -5,7 +5,7 @@ import User from '../api/models/user';
 
 module.exports = {
   resetRemaining: cron.schedule(
-    '* 0 * * *',
+    '0 0 * * *',
     () => User.resetRemaining()
       .then(ret => logger.info(`Reset remaining Cookiez for ${ret.nModified} users`))
       .catch(logger.error),
