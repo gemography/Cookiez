12abd3b0de1e56f69a1d3fdcdca91e1163e38d9c
aab11 <ahmed@monbanquet.fr>
2018-08-27 12:00:52 +0100

    Get the Cookiez balance of the requester only for now

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -2,18 +2,16 @@ import User from '../models/user';
 
 module.exports = {
   getCookiezAmount(req, res) {
-    const { token, user_name, text } = req.body;
+    const { token, user_id } = req.body;
     if (token !== process.env.SLACK_VERIFICATION_TOKEN) {
       return res.status(403).send('wrong token');
     }
-    const query = {};
-    if (text === '') query.name = user_name;
-    else query.name = text.substring(1);
 
+    const query = { userId: user_id };
     return User.findOne(query)
-      .then(u => res.send(!u
-        ? `${query.name} doesn't seem to be a Cookiez user`
-        : `You can still give ${u.remaining} today and have ${u.total} Cookiez in your balance`
-      ));
+      .then(u => {
+        const user = u || new User();
+        return res.send(`You can still give ${user.remaining} Cookiez today and have ${user.total} in your balance`)
+      });
   }
 };
